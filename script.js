c109a2f57e06450343e2c1486b354a4b51ec3e8e
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2023-11-02 13:08:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
     //Write your code here
-
+	let first=document.getElementById("first").value;
+	let last=document.getElementById("last").value;
+	alert(first+" "+last);
 }
